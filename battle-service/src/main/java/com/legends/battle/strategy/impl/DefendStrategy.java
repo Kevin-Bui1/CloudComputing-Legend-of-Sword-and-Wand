a2a0d189371c5be7fc3c726ee2226b3cc8b80965
Kevin Bui <kevinbui722@gmail.com>
2026-04-03 15:54:24 -0400

    Fix defendstrategy

--- battle-service/src/main/java/com/legends/battle/strategy/impl/DefendStrategy.java
+++ battle-service/src/main/java/com/legends/battle/strategy/impl/DefendStrategy.java
@@ -1,7 +1,9 @@
-javapackage com.legends.battle.strategy.impl;
+package com.legends.battle.strategy.impl;
+
 import com.legends.battle.Battle;
 import com.legends.battle.model.Unit;
 import com.legends.battle.strategy.BattleActionStrategy;
+
 public class DefendStrategy implements BattleActionStrategy {
     @Override public void execute(Battle battle, Unit actor) { battle.doDefend(actor); }
 }
